a2680fdbe21ad6de34496f38024490a73683150e
Luka Alves <49702498+LukaZSH@users.noreply.github.com>
2023-03-31 00:06:55 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-# Imersão Java Alura
-Neste primeiro projeto da imersão Java aprendemos a utilizar Api's, neste caso, utilizamos uma Api do IMDB de lista de filmes. 
-Pode-se listar desde os Top 250 filmes, os filmes mais populares, etc. Tudo de acordo com os rankings do IMDB.
-Em caso de dúvidas, confira o site: https://www.imdb.com 
+## Getting Started
 
-Por algumas complicações com a parte de Api's do IMDB junto da Amazon, o acesso à outras Api's do site fica restrito para maioria das pessoas,
-diante de tal fato, podemos utilizar estes links no "src" Java na parte de requisição HTTP :
-- https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json
-- https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json
-- https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json
-- https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json
+Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+
+## Folder Structure
+
+The workspace contains two folders by default, where:
+
+- `src`: the folder to maintain sources
+- `lib`: the folder to maintain dependencies
+
+Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+
+> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+
+## Dependency Management
+
+The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
---
+++ src/App.java
@@ -0,0 +1,68 @@
+import java.io.File;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {  
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão HTTP e buscar top filmes
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // pegar só os dados que interessam (Título, imagem do filme, classificação)
+        var parse = new JsonParser();
+        List<Map<String, String>> ListaDeFilmes = parse.parse(body);
+        
+        // criar um diretório de saída para as figurinhas
+        var diretorio = new File("figurinhas/");
+            diretorio.mkdir();
+
+        // exibir e manipular os dados
+        var geradora = new GeradorDeStickers();
+        for (int i = 0; i< 10; i++) {
+            Map<String,String> filme = ListaDeFilmes.get(i);
+            
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("fullTitle");
+
+            
+            InputStream inputStream = new URL(urlImagem).openStream();
+            
+            String nomeArquivo = "figurinhas/" + titulo + ".png";
+            // Não é possível criar nenhum arquivo no Windows com o caractere :
+            // Então os filmes que contém ":" no nome foram trocados por "-" para poder ser criado o arquivo .png
+            nomeArquivo = nomeArquivo.replace(":", "-");
+
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println("\u001b[1m Título:\u001b[m " + filme.get("fullTitle"));
+            System.out.println("\u001b[1m Link-Imagem:\u001b[m " + filme.get("image"));
+            double classificacao = Double.parseDouble(filme.get("imDbRating"));
+            int estrelas = (int) classificacao;
+            for (int n = 1; n <= estrelas; n++){
+                System.out.print("⭐");
+            }
+            
+            
+            System.out.print("\n");      
+            
+        }
+        
+    }
+}
